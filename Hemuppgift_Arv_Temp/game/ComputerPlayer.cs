eb7a3ab378dac411d25b545a332b2c9660dad80f
benjaminf-123 <bennefn@hotmail.com>
2024-10-14 19:58:08 +0200

    färdig

---
+++ Hemuppgift_Arv_Temp/game/ComputerPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.game
+{
+    public class ComputerPlayer : Player
+    {
+        private Random random;
+
+        public ComputerPlayer(string name) : base(name)
+        {
+            random = new Random();
+        }
+        public override int TakePins(Board board)
+        {
+            int pins = random.Next(1, 3);
+            Console.WriteLine($"{Name} tog {pins} stickor.");
+            return pins;
+
+        }
+    }
+}
---
+++ Hemuppgift_Arv_Temp/game/TakePins.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.game
+{
+    public class TakePinsGame
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            StartGame();
+        }
+
+        
+        public static void StartGame()
+        {
+            Board board = new Board(10); 
+
+            
+            Player player1 = new HumanPlayer("Spelare 1");
+            Player player2 = new ComputerPlayer("Datorn");
+
+            Player currentPlayer = player1; 
+            while (board.GetSticks() > 0)
+            {
+                Console.WriteLine($"Det finns {board.GetSticks()} stickor kvar. Den som drar sista stickan vinner");
+
+                
+                int takenPins = currentPlayer.TakePins(board);
+
+                
+                board.RemoveSticks(takenPins);
+
+                
+                if (board.GetSticks() == 0)
+                {
+                    Console.WriteLine($"{currentPlayer.Name} vinner!");
+                    break;
+                }
+
+                
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
+            }
+        }
+    }
+}
